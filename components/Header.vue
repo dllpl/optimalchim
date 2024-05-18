86154dfe42ebae8553826bfe333d447dc978fa62
dllpl <nick.iv.dev@gmail.com>
2024-05-18 22:56:32 +0300

    Таблетка заготовка

--- components/Header.vue
+++ components/Header.vue
@@ -1,3 +1,28 @@
+<script setup>
+const menu = [
+    {title: 'Главная', link: '/'},
+    {title: 'О компании', link: '/#about'},
+    {title: 'Продукция', link: '/#products'},
+    {title: 'Преимущества', link: '/#advantages'},
+    {title: 'Преминение', link: '/#application'},
+    {title: 'Контакты', link: '/#contacts'},
+]
+
+const contacts = [
+    {title: 'По вопросам и предложениям', link: 'mailto:info@optimalchim.ru', text: 'info@optimalchim.ru'},
+    {title: 'Для консультаций', link: 'tel:+79003292396', text: '+7 (900) 329-23-96'},
+]
+
+const menuOpen = ref(false);
+
+const toggleMenu = (is_burger = false) => {
+    console.log(menuOpen.value, is_burger)
+    if(menuOpen.value || is_burger) {
+        menuOpen.value = !menuOpen.value;
+        document.body.classList.toggle("dis-scroll");
+    }
+};
+</script>
 <template>
     <header class="header">
         <div class="container">
@@ -12,39 +37,22 @@
                     </div>
                 </NuxtLink>
                 <div class="header__right">
-                    <div class="header__email">
-                        <span>По вопросам и предложениям</span>
-                        <a href="mailto:info@optimalchim.ru" class="link link--accent link--700" title="Почта">info@optimalchim.ru</a>
+                    <div class="header__contact" v-for="(item, i) in contacts" :key="i">
+                        <span>{{item.title}}</span>
+                        <a :href="item.link" class="link link--accent link--700" :title="item.title">{{item.text}}</a>
                     </div>
-                    <div class="header__phone">
-                        <span>Для консультаций</span>
-                        <a href="tel:+79003292396" class="link link--accent link--700" title="Телефон">+7 (900) 329-23-96</a>
-                    </div>
+                    <button class="header__burger btn burger"
+                            @click="toggleMenu(true)"
+                    >
+                        <svg-icon :name="menuOpen ? 'close' : 'burger'" class="icon--burger"/>
+                    </button>
                 </div>
             </div>
             <div class="header__menu menu">
                 <nav class="header__nav nav">
                     <ul class="nav__list">
-                        <li class="nav__item">
-                            <NuxtLink to="#hero" class="nav__link">Главная</NuxtLink>
-                        </li>
-                        <li class="nav__item">
-                            <NuxtLink href="#about" class="nav__link">О компании</NuxtLink>
-                        </li>
-                        <li class="nav__item">
-                            <NuxtLink href="#products" class="nav__link">Продукция</NuxtLink>
-                        </li>
-                        <li class="nav__item">
-                            <NuxtLink href="#advantages" class="nav__link">Преимущества</NuxtLink>
-                        </li>
-                        <li class="nav__item">
-                            <NuxtLink href="#application" class="nav__link">Применение</NuxtLink>
-                        </li>
-                        <li class="nav__item">
-                            <NuxtLink href="#" class="nav__link">География поставок</NuxtLink>
-                        </li>
-                        <li class="nav__item">
-                            <NuxtLink href="#" class="nav__link">Контакты</NuxtLink>
+                        <li class="nav__item" v-for="(item, i) in menu" :key="i">
+                            <NuxtLink :to="item.link" class="nav__link">{{ item.title }}</NuxtLink>
                         </li>
                         <li class="nav__item">
                             <a href="tel:+79003292396" class="link link--flex" title="Позвонить">
@@ -54,6 +62,25 @@
                         </li>
                     </ul>
                 </nav>
+            </div>
+            <div class="header__menu-mobile" :class="menuOpen ? 'header__menu-mobile-active' : ''">
+                <nav class="header__nav nav">
+                    <ul class="nav__list-mobile">
+                        <li class="nav__item" v-for="(item, i) in menu" :key="i">
+                            <NuxtLink :to="item.link" class="nav__link" @click="toggleMenu(false)">{{ item.title }}</NuxtLink>
+                        </li>
+                        <li class="nav__item">
+                            <a href="tel:+79003292396" class="link link--flex" title="Позвонить" style="justify-content:start">
+                                <svg-icon name="phone" class="icon icon--nav-phone"/>
+                                <span class="link--accent">позвонить нам</span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <div class="header__contact-mobile" v-for="(item, i) in contacts" :key="i">
+                    <span>{{item.title}}</span>
+                    <a :href="item.link" class="link link--accent link--700" :title="item.title">{{item.text}}</a>
+                </div>
             </div>
         </div>
     </header>
